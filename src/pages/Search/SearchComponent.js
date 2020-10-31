d403f6b2c392477c5793f7067b51d86257af9ab8
Aleksandar Petic <aleksandar_petic@hotmail.com>
2020-10-31 14:36:22 +0100

    fix debounce in search component

--- src/pages/Search/SearchComponent.js
+++ src/pages/Search/SearchComponent.js
@@ -1,30 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { Header, Input, Segment } from 'semantic-ui-react'
-import { searchAPIDebounced } from '../../services/common/search'
+import { getSearchResults } from '../../services/common/search'
 import ArticlesList from '../Articles/ArticlesList'
+import { debounce } from 'lodash'
 
 const SearchComponent = (props) => {
 
-  const [results, setResults] = useState([])
+  const [searchData, setSearchData] = useState({
+    isBusy: false,
+    results: []
+  })
 
-  const handleSearch = async (e, data) => {
+  const { isBusy, results } = searchData
+
+  const handleSearch = debounce(async (text) => {
     try {
-      const result = await searchAPIDebounced(e.currentTarget.value)
-      setResults(result.data.articles)
+      setSearchData(prevData => ({ ...prevData, isBusy: true }))
+      const results = await getSearchResults(text)
+      setSearchData({
+        ...searchData,
+        results: results.data.articles,
+        isBusy: false
+      })
     } catch (error) {
-
+      setSearchData({
+        ...searchData,
+        results: [],
+        isBusy: false
+      })
     }
-  }
+  }, 300)
 
   return <Segment style={{ minHeight: '50vh', margin: '10px' }}>
     <Header>{'Search top news from GB by term'}</Header>
     <Input
-      onChange={(e, data) => handleSearch(e, data)}
+      loading={isBusy}
+      onChange={e => handleSearch(e.currentTarget.value)}
       icon='newspaper'
       iconPosition='left'
       style={{ marginTop: '50px', minWidth: '50vw', marginLeft: '20%' }}
       placeholder={'Search term...'} />
-    {results.length > 0 && <ArticlesList articles={results} />}
+    {results.length > 0 ? <ArticlesList articles={results} /> : null}
   </Segment>
 }
 export default SearchComponent
